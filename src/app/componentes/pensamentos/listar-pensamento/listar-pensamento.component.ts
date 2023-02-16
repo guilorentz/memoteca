bc3ca2d569d758da8cdb585f73fb26c350bd1217
Guilherme Lorentz <go.lorentz@gmail.com>
2023-02-16 13:52:13 -0400

    🎨 Adição da variação de largura

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,16 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
   listaPensamentos =[
-    // {
-    //   conteudo: 'Passo informações para o componente filho',
-    //   autoria: 'Componente pai',
-    //   modelo: 'modelo3'
-    // },
-    // {
-    //   conteudo: 'Minha propriedade é decorada com @Input()',
-    //   autoria: 'Componente filho',
-    //   modelo: 'modelo2'
-    // }
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: '"Não há nada permanente, exceto a mudança." - Heráclito. Este filósofo pré-socrático acreditava que o universo é regido pela mudança constante, e que a estabilidade e a permanência são ilusórias. Sua famosa citação nos lembra que tudo na vida está em constante evolução e transformação, e que é importante abraçar a mudança em vez de resistir a ela',
+      autoria: 'ChatGPT',
+      modelo: 'modelo1'
+    }
   ];
 
   constructor() { }
